0b27141b4a6ebc18a6cbd440f407d6cac85854a7
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-08-22 15:06:39 +0300

    commit tree dc20b0313e0e0727908c8372745d17f225dfb945

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
-
+class Book
+    attr_accessor :title, :author,:page_count, :genre
+    def initialize(title="And Then There Were None",author)
+        @title=title
+        @author =author
+        
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
